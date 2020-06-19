37d137385cf006596e430521434ebcdf594b0735
Homies886 <Niko876@hotmail.com>
2020-06-19 16:51:39 -0400

    Rendered JSON

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function getGames() {
   fetch(endPoint)
-    .then(res => res.json())
-    .then(json => console.log(json));
+    .then(response => response.json())
+    .then(games => {
+      games.data.forEach(game => {
+        const gameMarkup = `
+          <div data-id=${game.id}>
+            <img src=${game.attributes.image_url} height="200" width="250">
+            <h3>${game.attributes.title}</h3>
+            <p>${game.attributes.genre.name}</p>
+            <button data-id=${game.id}>edit</button>
+          </div>
+          <br><br>`;
+
+          document.querySelector('#game-container').innerHTML += gameMarkup
+      })
+    })
 }
