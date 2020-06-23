2e2e648d2d6c65c427e7cdd3ff82827f51395700
Homies886 <Niko876@hotmail.com>
2020-06-23 14:26:33 -0400

    Created form handler and post. Fixed index.html

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function getGames() {
       games.data.forEach(game => {
         const gameMarkup = `
           <div data-id=${game.id}>
-            <img src=${game.attributes.image_url} height="200" width="250">
+            <img src=${game.attributes.image_url} height="100" width="150">
             <h3>${game.attributes.title}</h3>
             <p>${game.attributes.genre.name}</p>
             <button data-id=${game.id}>edit</button>
@@ -25,4 +25,37 @@ function getGames() {
           document.querySelector('#game-container').innerHTML += gameMarkup
       })
     })
+}
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const titleInput = document.querySelector('#input-title').value
+  const descriptionInput = document.querySelector('#input-description').value
+  const imageInput = document.querySelector('#input-url').value
+  const genreId = parseInt(document.querySelector('#genres').value)
+  postFetch(titleInput, descriptionInput, imageInput, genreId)
+}
+
+function postFetch(title, description, image_url, genre_id) {
+  const bodyData = {title, description, image_url, genre_id}
+
+  fetch(endPoint, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(game => {
+    const gameData = game.data.attributes
+    const gameMarkup = `
+    <div data-id=${game.id}>
+      <img src=${gameData.image_url} height="100" width="150">
+      <h3>${gameData.title}</h3>
+      <p>${gameData.genre.name}</p>
+      <button data-id=${gameData.id}>edit</button>
+    </div>
+    <br><br>`;
+
+    document.querySelector('#game-container').innerHTML += gameMarkup;
+  })
 }
