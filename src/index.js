8b1f85f8f18594aaa96beddf666e0431a7d2e4d2
Homies886 <Niko876@hotmail.com>
2020-06-24 15:08:55 -0400

    Added event listener for delete button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const endPoint = "http://localhost:3000/api/v1/games";
+const gameEndPoint = "http://localhost:3000/api/v1/games/:id";
 
 document.addEventListener('DOMContentLoaded', () => {
   getGames()
@@ -6,6 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const createGameForm = document.querySelector("#create-game-form")
 
   createGameForm.addEventListener("submit", (e) => createFormHandler(e))
+
+  const gameContainer = document.querySelector('#game-container')
+  gameContainer.addEventListener('click', e => {
+    const id = parseInt(e.target.dataset.id);
+    const game = Game.findById(id);
+    game.removeGameCard(id);
+    deleteGame(game)
+  })
 })
 
 function getGames() {
@@ -46,5 +55,15 @@ function postFetch(title, description, image_url, genre_id) {
     let newGame = new Game(gameData, gameData.attributes)
 
     document.querySelector('#game-container').innerHTML += newGame.renderGameCard()
+  })
+}
+
+function deleteGame(info) {
+  const gameInfo = info
+
+  fetch(gameEndPoint, {
+    method: "DELETE",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(gameInfo)
   })
 }
