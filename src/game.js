f44db4d829952089b7f27e551c839b95bf81fdf8
Homies886 <Niko876@hotmail.com>
2020-06-24 15:09:15 -0400

    Added a removeCard function for delete

--- src/game.js
+++ src/game.js
@@ -1,25 +1,33 @@
 class Game {
 
-    constructor(game, gameAttributes) {
-      this.id = game.id
-      this.title = gameAttributes.title
-      this.description = gameAttributes.description
-      this.image_url = gameAttributes.image_url
-      this.genre = gameAttributes.genre
-      Game.all.push(this)
-    }
-  
-    renderGameCard() {
-      return `
-        <div data-id=${this.id}>
-          <img src=${this.image_url} height="200" width="250">
-          <h3>${this.title} (${this.genre.name})</h3>
-          <p>${this.description}</p>
-          <button data-id=${this.id}>Delete</button>
-        </div>
-        <br><br>`;
-    }
-  
+  constructor(game, gameAttributes) {
+    this.id = game.id
+    this.title = gameAttributes.title
+    this.description = gameAttributes.description
+    this.image_url = gameAttributes.image_url
+    this.genre = gameAttributes.genre
+    Game.all.push(this)
   }
   
-  Game.all = [];
+  renderGameCard() {
+    return `
+      <div data-id=${this.id}>
+        <img src=${this.image_url} height="200" width="250">
+        <h3>${this.title} (${this.genre.name})</h3>
+        <p>${this.description}</p>
+        <button data-id=${this.id}>Delete</button>
+      </div>
+      <br><br>`;
+  }
+
+  removeGameCard(id) {
+    const el = document.querySelector(`[data-id='${id}']`);
+    el.remove()
+  }
+
+  static findById(id) {
+    return this.all.find(game => game.id === id.toString());
+  }
+}
+
+Game.all = [];
